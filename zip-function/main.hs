8369738d72511563a73f507b52c36d377e38f71d
carlos-dani-dev <carlos.rocha@ufpi.edu.br>
2026-06-08 12:33:49 -0300

    'verificação de inteiro' question solved

--- zip-function/main.hs
+++ zip-function/main.hs
@@ -1,1 +1,16 @@
 -- respostas às questões em [exercício de ZF.txt]
+
+--questão 1
+teste_q1 :: [Int]
+teste_q1 = [1, 2, -1, -10, 0, 0, 1]
+
+verifica :: Int -> String
+verifica a
+    |a < 0 = "Negativo"
+    |a == 0 = "Zero"
+    |a > 0 = "Positivo"
+
+q1 :: [Int] -> [(Int, String)]
+q1 lista = [(a, verifica(a)) | a <- lista]
+
+--questão 2
